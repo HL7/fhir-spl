f2f6c4c8e5fd8e62a12fbc7f5888c6930e87f666
Jean Duteau <jean@duteaudesign.com>
2024-12-10 01:54:55 -0600

    Add new npm package.

--- makeNPMPackage.sh
+++ makeNPMPackage.sh
@@ -1,3 +1,5 @@
+version=`sed -En 's/  "original-version" : "([0-9]\.[0-9]\.[0-9])",/\1/p' ~/.fhir/packages/hl7.fhir.us.spl\#dev/package/package.json`
+sed -Ei '' 's/  "version" : "dev"/  "version" : "'"$version"'"/' ~/.fhir/packages/hl7.fhir.us.spl\#dev/package/package.json
 cd ~/.fhir/packages/hl7.fhir.us.spl\#dev
 tar -czvf ~/Documents/DDIWork/Samvit/SPL/fhir-spl/package/hl7.fhir.us.spl.tgz .
 cd -
